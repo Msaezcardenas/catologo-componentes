ec790d39feb1d5e8a2ec91b4e58f05aab97d0b76
molu <msaezcardenas@gmail.com>
2024-04-18 20:17:45 -0400

    firt commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Layout.css';
+
+function Layout() {
+  return (
+    <div className='layout'>
+      <div className='sidebar'> parte izquierda</div>
+      <div className='page'> parte izquierda</div>
+    </div>
+  );
+}
+
+export default Layout;
